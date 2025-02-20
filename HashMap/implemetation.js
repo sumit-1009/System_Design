908b75903281ed68b11e5ff42063723ce87cc266
Sumit Sahu <sssahu1009@gmail.com>
2025-02-20 23:22:19 +0530

    Added has method in hashmap

--- HashMap/implemetation.js
+++ HashMap/implemetation.js
@@ -6,55 +6,66 @@ function hashStringToInt(s, tableSize){
     return hash;
 }
 
-class HashTable{
-    constructor(){
-        this.table = new Array(17);
-        this.size = 0;
-        this.threshold = 0.75;
-    }
-    loadFactor() {
-        return this.size / this.table.length;
-    }
-    reSize(){
-        let newTableSize = this.table.length * 2;
-        let newTable = new Array(newTableSize);
-        for(let i=0; i<this.table.length; i++){
-            if(this.table[i]){
-                for(const [key, value] of this.table[i]){
-                    let idx = hashStringToInt(key, newTableSize);
-                    if(newTable){
-                        newTable[idx].push([key, value]);
-                    } else{
-                        newTable[idx] = [[key, value]]
-                    }
-                }
-            }
+class HashTable {
+  constructor() {
+    this.table = new Array(17);
+    this.size = 0;
+    this.threshold = 0.75;
+  }
+  loadFactor() {
+    return this.size / this.table.length;
+  }
+  reSize() {
+    let newTableSize = this.table.length * 2;
+    let newTable = new Array(newTableSize);
+    for (let i = 0; i < this.table.length; i++) {
+      if (this.table[i]) {
+        for (const [key, value] of this.table[i]) {
+          let idx = hashStringToInt(key, newTableSize);
+          if (newTable) {
+            newTable[idx].push([key, value]);
+          } else {
+            newTable[idx] = [[key, value]];
+          }
         }
-        this.table = newTable;
+      }
     }
-    setItem = (key, value) => {
-        let idx = hashStringToInt(key, this.table.length);
-        if(this.table[idx]){
-            this.table[idx].push([key, value]);
-        } else{
-            this.table[idx] = [[key, value]];
-        }
-        this.size++;
-        if(this.loadFactor() > this.threshold){
-            reSize();
-        }
+    this.table = newTable;
+  }
+  setItem = (key, value) => {
+    let idx = hashStringToInt(key, this.table.length);
+    if (this.table[idx]) {
+      this.table[idx].push([key, value]);
+    } else {
+      this.table[idx] = [[key, value]];
     }
-    getItem = (key, value) => {
-        let idx = hashStringToInt(key, this.table.length);
-        if(this.table[idx]){
-            for(let i=0; i<this.table[idx].length; i++){
-                if(this.table[idx][i][0] === key){
-                    return this.table[idx][i][1];
-                }
-            }
-        }
-        return undefined;
+    this.size++;
+    if (this.loadFactor() > this.threshold) {
+      reSize();
     }
+  };
+  getItem = (key, value) => {
+    let idx = hashStringToInt(key, this.table.length);
+    if (this.table[idx]) {
+      for (let i = 0; i < this.table[idx].length; i++) {
+        if (this.table[idx][i][0] === key) {
+          return this.table[idx][i][1];
+        }
+      }
+    }
+    return undefined;
+  };
+  hasItem = (key, value) => {
+    let idx = hashStringToInt(key, this.table.length);
+    if (this.table[idx]) {
+      for (let i = 0; i < this.table[idx].length; i++) {
+        if (this.table[idx][i][0] === key) {
+          return true;
+        }
+      }
+    }
+    return false;
+  };
 }
 let hashTable = new HashTable();
 hashTable.setItem("apple", 10);
@@ -66,3 +77,4 @@ console.log(hashTable.getItem("banana")); // Output: 20
 console.log(hashTable.getItem("orange")); // Output: 30
 console.log(hashTable.getItem("grape")); // Output: 40
 console.log(hashTable.getItem("mango")); // Output: undefined
+console.log(hashTable.hasItem("apple"));
